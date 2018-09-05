606219ea8bb96d2ae987ca51fd554de6f78a153d
MuppidiBhavana <muppidi.bhavana@gmail.com>
2018-09-05 20:47:02 +0530

    static

---
+++ static/js/file.js
@@ -0,0 +1,14 @@
+function f1() {
+    if (document.getElementById("n_account").style.display == 'none' && document.getElementById("e_account").style.display == 'none')
+    {
+        document.getElementById("n_account").style.display = 'block';
+        document.getElementById("e_account").style.display = 'block';
+
+    }
+    else
+    {
+        document.getElementById("n_account").style.display = 'none';
+        document.getElementById("e_account").style.display = 'none';
+
+    }
+}
